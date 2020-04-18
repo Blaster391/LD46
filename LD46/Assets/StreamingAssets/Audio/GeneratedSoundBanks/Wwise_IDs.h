260c8e07f99bfbd36b801a938d578c4f529985a8
Daniel Matarov <daniel.matarov@gmail.com>
2020-04-18 17:13:44 +0100

    Turret sounds

--- LD46/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ LD46/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,7 +14,24 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID BALLOFLIGHT = 2769336799U;
+        static const AkUniqueID TURRET = 2674766611U;
+        static const AkUniqueID TURRET2 = 212920027U;
     } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace TURRETTYPE
+        {
+            static const AkUniqueID GROUP = 1521021787U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID HIGH = 3550808449U;
+                static const AkUniqueID LOW = 545371365U;
+            } // namespace SWITCH
+        } // namespace TURRETTYPE
+
+    } // namespace SWITCHES
 
     namespace BANKS
     {
