d9cc8adea75c75c004acbc754c13373d94f9ae21
Robert Arnold <robertarnold391@gmail.com>
2020-04-18 18:28:55 +0100

    Audio event stuff for Dan

---
+++ LD46/Assets/Scripts/AudioEventManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioEventManager : MonoBehaviour
+{
+    [SerializeField]
+    AudioEventPosition m_audioEventPrefab;
+
+    public AudioEventPosition MakeAudioEvent(Vector2 position, float lifespan, AK.Wwise.Event wwiseEvent)
+    {
+        var audioEventObject = Instantiate<AudioEventPosition>(m_audioEventPrefab);
+        audioEventObject.SetLifespan(lifespan);
+        wwiseEvent.Post(audioEventObject.gameObject);
+        return audioEventObject;
+    }
+}
