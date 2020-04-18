6415d127750a6bf3527747ba895b0e6fa3e48eaa
Robert Arnold <robertarnold391@gmail.com>
2020-04-18 11:18:35 +0100

    Basic path finding and enemies

---
+++ LD46/Assets/Scripts/AI/Enemy.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_defaultTarget;
+
+    [SerializeField]
+    private float m_moveForce = 1.0f;
+
+    private Rigidbody2D m_rigidbody2D = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        Vector2 targetPosition = m_defaultTarget.transform.position;
+        Vector2 myPosition = transform.position;
+        Vector2 directionToTarget = new Vector2();
+
+        NavMesh mesh = GameHelper.GetManager<NavMesh>();
+        var path = mesh.RequestPath(myPosition, targetPosition);
+
+        if(path != null && path.Count > 2)
+        {
+            directionToTarget = (path[2] - myPosition).normalized;
+            _debugPath = path;
+        }
+        else
+        {
+            directionToTarget = (targetPosition - myPosition).normalized;
+        }
+
+        m_rigidbody2D.AddForce(directionToTarget * m_moveForce);
+    }
+
+    List<Vector2> _debugPath = null;
+    private void OnDrawGizmosSelected()
+    {
+        NavMesh mesh = GameHelper.GetManager<NavMesh>();
+        mesh.DebugDrawPath(_debugPath);
+    }
+
+}
---
+++ LD46/Assets/Scripts/AI/PathFinding/PathNode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNode
+{
+    public Vector2Int Index { get; private set; }
+    public Vector2 Position { get; private set; }
+    public List<PathNode> ConnectedNodes { get; private set; } = new List<PathNode>();
+
+    public PathNode(Vector2 position, Vector2Int index)
+    {
+        Position = position;
+        Index = index;
+    }
+
+    public void AddConnectedNodes(PathNode node)
+    {
+        ConnectedNodes.Add(node);
+    }
+}
