c5877a0301faf71f427b66661b94afe299e20eef
Jack Hardy <jaaronhardy@gmail.com>
2020-04-18 14:33:17 +0100

    Added 'Drainable' behaviour and visual component along with it

---
+++ LD46/Assets/Scripts/Drainable Objects/DrainableBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Something that can be drained by THE ORB
+public class DrainableBehaviour : MonoBehaviour
+{
+    [SerializeField] private float m_maxEnergy;
+
+    public float CurrentEnergy { get; private set; }
+    public float MaxEnergy { get { return m_maxEnergy; } }
+    public float CurrentEnergyProp { get { return CurrentEnergy / m_maxEnergy; } }
+
+    // Callbacks for visuals if we separate
+    public System.Action<DrainableBehaviour> EnergyChange = delegate { }; // Energy remaining, energy prop remaining
+    public System.Action Dead = delegate { };
+
+    // Static tracking
+    private static List<DrainableBehaviour> s_drainables = new List<DrainableBehaviour>();
+    public static List<DrainableBehaviour> Drainables { get { return new List<DrainableBehaviour>(s_drainables); } }
+    
+    void Start()
+    {
+        s_drainables.Add(this);
+
+        CurrentEnergy = m_maxEnergy;
+    }
+
+    void OnDestroy()
+    {
+        s_drainables.Remove(this);
+    }
+    
+    // Interface
+    public float TakeEnergy(float _energy)
+    {
+        float previousEnergy = CurrentEnergy;
+        CurrentEnergy = Mathf.Clamp(CurrentEnergy - _energy, 0f, m_maxEnergy);
+        
+        EnergyChange(this);
+        if(CurrentEnergy == 0f)
+        {
+            Die();
+        }
+        return previousEnergy - CurrentEnergy;
+    }
+
+    private void Die()
+    {
+        Dead();
+
+        // Kill self? Maybe not idk yet
+        Destroy(gameObject);
+    }
+}
---
+++ LD46/Assets/Scripts/Drainable Objects/DrainableVisualControlBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(DrainableBehaviour))]
+public class DrainableVisualControlBehaviour : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer m_spriteRenderer;
+
+    [Header("Colour")]
+    [SerializeField] private bool m_useSpriteRenderableColour = false;
+    [SerializeField] private Color m_fullHealthColour = Color.white;
+    [SerializeField] private Color m_noHealthColour = Color.clear;
+
+    [Header("Scale")]
+    [SerializeField] private float m_fullhealthScale = 1f;
+    [SerializeField] private float m_noHealthScale = 0.3f;
+
+    Vector3 m_originalScale = Vector3.one;
+
+    void Start()
+    {
+        GetComponent<DrainableBehaviour>().EnergyChange += UpdateVisuals;
+        if(m_useSpriteRenderableColour)
+        {
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+            if(spriteRenderer)
+            {
+                m_fullHealthColour = spriteRenderer.color;
+            }
+        }
+
+        m_originalScale = transform.localScale;
+    }
+
+    void UpdateVisuals(DrainableBehaviour _drainable)
+    {
+        m_spriteRenderer.color = Color.Lerp(m_noHealthColour, m_fullHealthColour, _drainable.CurrentEnergyProp);
+        float scale = Mathf.Lerp(m_noHealthScale, m_fullhealthScale, _drainable.CurrentEnergyProp);
+        transform.localScale = m_originalScale * scale;
+    }
+}
