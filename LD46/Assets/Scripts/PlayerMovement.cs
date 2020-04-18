558e055b75a2434ef3aaf0fac9595b4ae21716b4
Karl Davis <karl@karltechno.com>
2020-04-18 11:01:32 +0100

    Grounded circle cast

--- LD46/Assets/Scripts/PlayerMovement.cs
+++ LD46/Assets/Scripts/PlayerMovement.cs
@@ -11,19 +11,52 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float m_baseJumpForce = 1.0f;
 
+    [SerializeField]
+    private float m_groundedEpsilon = 0.05f;
+
 
     private Rigidbody2D m_rigidbody2D = null;
+    private CapsuleCollider2D m_capsuleCollider2D = null;
+
+    private RaycastHit2D m_lastGroundHit;
+    private bool m_isGrounded = true;
 
     // Start is called before the first frame update
     void Start()
     {
         m_rigidbody2D = GetComponent<Rigidbody2D>();
+        m_capsuleCollider2D = GetComponent<CapsuleCollider2D>();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(m_lastGroundHit)
+        {
+            Gizmos.color = m_isGrounded ? Color.green : Color.red;
+            Gizmos.DrawSphere(new Vector3(m_lastGroundHit.point.x, m_lastGroundHit.point.y, 0.0f), 0.1f);
+        }
+    }
+
+    private void DoGroundCast()
+    {
+        var capsuleBounds = m_capsuleCollider2D.bounds;
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(m_rigidbody2D.position, capsuleBounds.extents.x, Vector2.down);
+        foreach (var hit in hits)
+        {
+            if (hit.rigidbody == m_rigidbody2D) continue;
+
+            m_lastGroundHit = hit;
+            m_isGrounded = (m_rigidbody2D.position.y - hit.point.y) >= (capsuleBounds.extents.y + m_groundedEpsilon);
+            break;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.A))
+        DoGroundCast();
+
+        if (Input.GetKey(KeyCode.A))
         {
             m_rigidbody2D.AddForce(-Vector2.right * m_baseMovementForce);
         }
@@ -36,8 +69,7 @@ public class PlayerMovement : MonoBehaviour
         {
             m_rigidbody2D.AddForce(Vector2.up * m_baseJumpForce, ForceMode2D.Impulse);
         }
-
-
+        
         float speed = m_rigidbody2D.velocity.magnitude;
         if (speed > m_maxVelocity)
         {
