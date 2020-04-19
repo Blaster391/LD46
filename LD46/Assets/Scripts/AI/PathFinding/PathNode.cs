d5866ea13718b1818808bacc82d210c159107ca1
Robert Arnold <robertarnold391@gmail.com>
2020-04-19 15:23:06 +0100

    Load and save navmesh

--- LD46/Assets/Scripts/AI/PathFinding/PathNode.cs
+++ LD46/Assets/Scripts/AI/PathFinding/PathNode.cs
@@ -18,4 +18,71 @@ public class PathNode
     {
         ConnectedNodes.Add(node);
     }
+
+    public PathNodeSerializable GetSerializable()
+    {
+        PathNodeSerializable node = new PathNodeSerializable();
+        node.Index = new Vector2IntSerializable(Index);
+        node.Position = new Vector2Serializable(Position);
+        foreach(var n in ConnectedNodes)
+        {
+            node.ConnectedNodes.Add(new Vector2IntSerializable(n.Index));
+        }
+
+        return node;
+    }
+}
+
+public class Vector2IntSerializable
+{
+    public Vector2IntSerializable()
+    {
+
+    }
+
+    public Vector2IntSerializable(Vector2Int val)
+    {
+        X = val.x;
+        Y = val.y;
+    }
+
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Vector2Int Get()
+    {
+        return new Vector2Int(X, Y);
+    }
+}
+
+public class Vector2Serializable
+{
+    public Vector2Serializable()
+    {
+
+    }
+
+    public Vector2Serializable(Vector2 val)
+    {
+        X = val.x;
+        Y = val.y;
+    }
+
+
+    public float X { get; set; }
+    public float Y { get; set; }
+
+    public Vector2 Get()
+    {
+        return new Vector2(X, Y);
+    }
+}
+
+
+public class PathNodeSerializable
+{
+    public Vector2IntSerializable Index { get; set; }
+    public Vector2Serializable Position { get; set; }
+    public List<Vector2IntSerializable> ConnectedNodes { get; set; } = new List<Vector2IntSerializable>();
+
 }
