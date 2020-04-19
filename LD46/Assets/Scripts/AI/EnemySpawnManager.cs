0a0b93f013ceb4a378894eb1908f1af3766f6261
Jack Hardy <jaaronhardy@gmail.com>
2020-04-19 09:08:45 +0100

    Adding variable enemy spawning in waves

--- LD46/Assets/Scripts/AI/EnemySpawnManager.cs
+++ LD46/Assets/Scripts/AI/EnemySpawnManager.cs
@@ -3,36 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+[System.Serializable]
+public class EnemySpawnWeight
+{
+    public Enemy m_enemyPrefab;
+    public float m_weight;
+}
+
+[System.Serializable]
+public class EnemySpawnWaveSetting
+{
+    public float m_waveDuration = 30f;
+    public List<EnemySpawnWeight> m_enemySpawnWeights;
+    public float m_spawnRate = 10f;
+    public int m_maxEnemies = 10;
+
+    public Enemy GetEnemy()
+    {
+        float totalWeight = 0;
+        foreach(EnemySpawnWeight spawnWeight in m_enemySpawnWeights)
+        {
+            totalWeight += spawnWeight.m_weight;
+        }
+
+        float selectedWeight = Random.Range(0f, totalWeight);
+
+        float currentTotalWeight = 0f;
+        foreach (EnemySpawnWeight spawnWeight in m_enemySpawnWeights)
+        {
+            currentTotalWeight += spawnWeight.m_weight;
+            if(selectedWeight < currentTotalWeight)
+            {
+                return spawnWeight.m_enemyPrefab;
+            }
+        }
+        return null;
+    }
+}
+
 public class EnemySpawnManager : MonoBehaviour
 {
     [SerializeField]
-    private GameObject m_enemy = null;
-    [SerializeField]
-    private float m_spawnRate = 10.0f;
-    [SerializeField]
-    private int m_maxEnemies = 10;
+    private List<EnemySpawnWaveSetting> m_enemySpawnWaveSettings = new List<EnemySpawnWaveSetting>();
 
+    private int m_currentWaveIndex = 0;
+    private float m_currentWaveDuration = 0f;
 
     private float m_lastSpawnTime = 0.0f;
-    void Start()
-    {
         
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if(GameObject.FindObjectsOfType<Enemy>().Length >= m_maxEnemies)
+        m_currentWaveDuration += Time.deltaTime;
+        if(m_currentWaveDuration > m_enemySpawnWaveSettings[m_currentWaveIndex].m_waveDuration)
+        {
+            // If theres no more wave definitions we'll just keep going with the current one
+            if (m_currentWaveIndex + 1 < m_enemySpawnWaveSettings.Count)
+            {
+                ++m_currentWaveIndex;
+            }
+        }
+
+        if(Enemy.Enemies.Count >= GetCurrentWave().m_maxEnemies)
         {
             return;
         }
 
         m_lastSpawnTime += Time.deltaTime;
-        if(m_lastSpawnTime > m_spawnRate)
+        if(m_lastSpawnTime > GetCurrentWave().m_spawnRate)
         {
             m_lastSpawnTime = 0.0f;
 
-            var spawners = GameObject.FindObjectsOfType<EnemySpawner>().Where(x => x.IsAvailable).ToList();
+            var spawners = EnemySpawner.EnemySpawners.Where(x => x.IsAvailable).ToList();
             if (spawners.Count > 0)
             {
                 int index = Mathf.RoundToInt(Random.Range(0, spawners.Count));
@@ -43,7 +84,18 @@ public class EnemySpawnManager : MonoBehaviour
 
     void SpawnAtSpawner(EnemySpawner spawner)
     {
-        var newEnemy = Instantiate<GameObject>(m_enemy);
+        var newEnemy = Instantiate(GetEnemy());
         newEnemy.transform.position = spawner.transform.position;
+    }
+
+    EnemySpawnWaveSetting GetCurrentWave()
+    {
+        return m_enemySpawnWaveSettings[m_currentWaveIndex];
+    }
+
+    Enemy GetEnemy()
+    {
+        EnemySpawnWaveSetting waveSetting = GetCurrentWave();
+        return waveSetting.GetEnemy();
     }
 }
