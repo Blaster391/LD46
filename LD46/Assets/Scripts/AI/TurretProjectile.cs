c58041ce87fa79e55ae5bdcf32adedbec821b7e1
Robert Arnold <robertarnold391@gmail.com>
2020-04-20 10:03:06 +0100

    Rework score metric

--- LD46/Assets/Scripts/AI/TurretProjectile.cs
+++ LD46/Assets/Scripts/AI/TurretProjectile.cs
@@ -47,9 +47,6 @@ public class TurretProjectile : MonoBehaviour
         Enemy enemy = collision.gameObject.GetComponent<Enemy>();
         if (enemy != null)
         {
-            GameHelper.GetManager<StatsManager>().IncrementKills();
-            GameHelper.GetManager<StatsManager>().AddScore(1);
-
             enemy.DealDamage(m_damage);
             Destroy(gameObject);
         }
--- LD46/Assets/Scripts/StatsManager.cs
+++ LD46/Assets/Scripts/StatsManager.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class StatsManager : MonoBehaviour
 {
+    [SerializeField]
+    private float m_killScoreMod = 1.0f;
+    [SerializeField]
+    private float m_timeScoreMod = 0.1f;
+    [SerializeField]
+    private float m_drainScoreMod = 0.05f;
 
     public float EnergyLeached { get; private set; } = 0;
-    public int Score { get; private set; } = 0;
+    public int Score 
+    { 
+        get
+        {
+            return Mathf.RoundToInt(Kills * m_killScoreMod + TimeAlive * m_timeScoreMod + EnergyLeached * m_drainScoreMod);
+        }
+    } 
     public int Kills { get; private set; } = 0;
     public float TimeAlive { get; private set; } = 0;
 
@@ -31,14 +43,6 @@ public class StatsManager : MonoBehaviour
         if (IsAlive)
         {
             Kills++;
-        }
-    }
-
-    public void AddScore(int amount)
-    {
-        if (IsAlive)
-        {
-            Score += amount;
         }
     }
 
